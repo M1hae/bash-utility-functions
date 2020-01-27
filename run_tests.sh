9b5410e9faf543444ec5df2c3693551aa53d3824
Michael Zellner <Michael@ze-da.de>
2020-01-27 19:57:05 +0100

    Implemented basic string functions

--- run_tests.sh
+++ run_tests.sh
@@ -5,19 +5,15 @@ source logging/src/logging.bash
 
 bats='thirdparty/bats/bin/bats'
 
-Info "Single argument tests"
-${bats} logging/test/single_argument_tests.bash
+function ExecuteTest()
+{
+    Info "Executing ${1}:"
+    ${bats} ${1}
+}
 
-Info "Multiple argument tests"
-${bats} logging/test/multiple_arguments_tests.bash
-
-Info "Format and color tests"
-${bats} logging/test/format_color_tests.bash
-
-Info "Die function tests"
-${bats} logging/test/die_tests.bash
-
-Info "Log level tests"
-${bats} logging/test/log_level_tests.bash
-
-
+ExecuteTest "logging/test/single_argument_tests.bash"
+ExecuteTest "logging/test/multiple_arguments_tests.bash"
+ExecuteTest "logging/test/format_color_tests.bash"
+ExecuteTest "logging/test/die_tests.bash"
+ExecuteTest "logging/test/log_level_tests.bash"
+ExecuteTest "string/test/string_tests.bash"
